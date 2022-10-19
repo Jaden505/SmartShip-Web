1dcd5f85b964652cbf9d406dc95b6fecafc4fcf2
Vincent <vincent.ohr@hva.nl>
2022-10-19 23:24:33 +0200

    springboot start

---
+++ smartship/src/test/java/com/example/smartship/SmartshipApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartship;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartshipApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
