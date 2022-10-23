84ff7c2274d9dcf39906938fb0e57deb7fff2959
Raihan <raihan.saboerali@hva.nl>
2022-10-23 16:10:26 +0200

    clock-in layout

---
+++ frontend/src/components/Ships.vue
@@ -0,0 +1,99 @@
+<template>
+  <div class="card" v-for="(ship, index) in ships" :key="index">
+    <div class="row">
+      <div class="img-box">
+        <p>{{ ship.shipName }}</p>
+      </div>
+      <div class="infoBox">
+        <p class="shipTitle">Ship name: {{ ship.shipName }}</p>
+        <p class="shipTitle">Ship description: {{ ship.shipDesc }}</p>
+      </div>
+    </div>
+    <div class="clock-in">
+      <p>Clock In</p>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "ShipsComponent",
+
+  data(){
+    return {
+      ships: [
+        {
+          shipName: 'Titanic',
+          shipDesc: 'This is a safe ship'
+        },
+        {
+          shipName: 'The Mayflower',
+          shipDesc: 'Beautiful ship'
+        },
+        {
+          shipName: 'Cruise Ship 1997',
+          shipDesc: 'A luxurious ship'
+        }
+      ]
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+.card{
+  border: 2px solid black;
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: #33ccff;
+  margin-bottom: 1%;
+}
+
+.row{
+  display: flex;
+}
+
+.img-box{
+  flex: 1;
+  font-size: 40px;
+  text-align: center;
+  border: 2px solid black;
+  margin: 1%;
+  background-color: white;
+}
+
+.infoBox{
+  text-align: left;
+  flex: 3;
+  margin: 1%;
+  padding: 1%;
+  border: 2px solid black;
+  background-color: white;
+}
+
+.shipTitle{
+  font-weight: bold;
+}
+
+.clock-in p{
+  position: relative;
+  left: 87%;
+  width: 10%;
+  margin: 0 1% 1% 0;
+  background-color: white;
+  border: 2px solid black;
+  border-radius: 20px;
+  padding: 1%;
+  cursor: pointer;
+}
+
+.clock-in p:hover{
+  background-color: #80bfff;
+}
+
+
+</style>
