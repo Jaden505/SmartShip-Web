12c05f2aca46fcad6170324d309c1588d3b59766
Raihan <raihan.saboerali@hva.nl>
2022-10-23 16:42:08 +0200

    hardcoded dummy data

--- frontend/src/components/Ships.vue
+++ frontend/src/components/Ships.vue
@@ -1,19 +1,33 @@
 <template>
-  <div class="card" v-for="(ship, index) in ships" :key="index">
-    <div class="row">
-      <div class="img-box">
-        <p>{{ ship.shipName }}</p>
+  <div v-if="active">
+    <label>OperatorId:</label><br/>
+    <select v-model="operatorId">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+    </select>
+
+    <div class="card" v-for="(ship, index) in ships" :key="index">
+      <div class="row">
+        <div class="img-box">
+          <p>{{ ship.shipName }}</p>
+        </div>
+        <div class="infoBox">
+          <p class="shipTitle">Ship id: {{ ship.shipId }}</p>
+          <p class="shipTitle">Ship name: {{ ship.shipName }}</p>
+          <p class="shipTitle">Ship description: {{ ship.shipDesc }}</p>
+        </div>
       </div>
-      <div class="infoBox">
-        <p class="shipTitle">Ship name: {{ ship.shipName }}</p>
-        <p class="shipTitle">Ship description: {{ ship.shipDesc }}</p>
+      <div class="clock-in">
+       <p @click="clockIn(ship.shipId)">Clock In</p>
       </div>
-    </div>
-    <div class="clock-in">
-      <p>Clock In</p>
     </div>
   </div>
-
+  <div v-else>
+    <h1>{{ message }}</h1>
+  </div>
 </template>
 
 <script>
@@ -24,18 +38,30 @@ export default {
     return {
       ships: [
         {
+          shipId: '1',
           shipName: 'Titanic',
           shipDesc: 'This is a safe ship'
         },
         {
+          shipId: '2',
           shipName: 'The Mayflower',
           shipDesc: 'Beautiful ship'
         },
         {
+          shipId: '3',
           shipName: 'Cruise Ship 1997',
           shipDesc: 'A luxurious ship'
         }
-      ]
+      ],
+      operatorId: '',
+      active: true,
+      message: ''
+    }
+  },
+  methods: {
+    clockIn(shipId){
+      this.active = false
+      this.message = 'Operator: ' + this.operatorId + ' clocked in at ShipId: ' + shipId
     }
   }
 
@@ -51,6 +77,7 @@ export default {
   margin-right: auto;
   background-color: #33ccff;
   margin-bottom: 1%;
+  border-radius: 20px;
 }
 
 .row{
@@ -83,6 +110,7 @@ export default {
   position: relative;
   left: 87%;
   width: 10%;
+  text-align: center;
   margin: 0 1% 1% 0;
   background-color: white;
   border: 2px solid black;
@@ -93,6 +121,21 @@ export default {
 
 .clock-in p:hover{
   background-color: #80bfff;
+}
+
+label{
+  color: white;
+  margin-left: 1%;
+}
+
+select{
+  margin-left: 1%;
+  text-align: center;
+  width: 6%;
+}
+
+h1{
+  color: white;
 }
 
 
