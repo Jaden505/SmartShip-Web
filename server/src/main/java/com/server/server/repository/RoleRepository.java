ab9bc558e3a51e33aab84a3c69806b5d1fe9ed5b
Bartek Tynior <btynior@gmail.com>
2022-10-23 18:38:42 +0200

    Fix for database

--- server/src/main/java/com/server/server/repository/RoleRepository.java
+++ server/src/main/java/com/server/server/repository/RoleRepository.java
@@ -2,7 +2,9 @@ package com.server.server.repository;
 
 import com.server.server.domain.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Role findByName(String name);
 
