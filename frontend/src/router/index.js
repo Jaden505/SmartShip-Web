50d7cebe69c3130ac58c81978046c73551f8499d
Raihan <raihan.saboerali@hva.nl>
2022-10-23 16:10:26 +0200

    clock-in layout

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,9 +1,11 @@
 import {createRouter, createWebHashHistory} from "vue-router";
 import Login from "@/components/Login";
+import Ships from "@/components/Ships";
 
 const routes = [
     {path: "/",
-        component: Login}
+        component: Login},
+    { path: "/ships", component: Ships}
 ];
 
 export const router = createRouter({
