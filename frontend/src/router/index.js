997bdd1d969fd3dde1452a6e7bd5b1d3dd2fca53
Jaden505 <jadenxxxl@gmail.com>
2022-10-24 17:42:34 +0200

    stash

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,9 +2,11 @@ import {createRouter, createWebHashHistory} from "vue-router";
 import Login from "@/components/Login";
 import PowerUsage from "@/components/ship/PowerUsage";
 import ShipInfo from "@/components/ship/Information";
+import ListShips from "@/components/ship/ListShips";
 
 const routes = [
     {path: "/", component: Login},
+    {path : "/ships/", component: ListShips},
     {path : "/ships/power-usage", component: PowerUsage},
     {path : "/ships/info", component: ShipInfo},
 ];
