55ee10e6d51276c3fb3bec81f25f6afe0fcc75af
Jaden505 <jadenxxxl@gmail.com>
2022-10-24 21:01:30 +0200

    Ships overview added not connected to database.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -2,16 +2,13 @@ import {createRouter, createWebHashHistory} from "vue-router";
 import Login from "@/components/Login";
 import PowerUsage from "@/components/ship/PowerUsage";
 import ShipInfo from "@/components/ship/Information";
-import ListShips from "@/components/ship/ListShips";
-import Clockin from "@/components/Clockin";
+import Ships from "@/components/Ships";
 
 const routes = [
     {path: "/", component: Login},
-    {path : "/ships/", component: ListShips},
+    {path : "/ships/", component: Ships},
     {path : "/ships/power-usage", component: PowerUsage},
     {path : "/ships/info", component: ShipInfo},
-    {path: "/", component: Login},
-    {path: "/Clockin", component: Clockin}
 ]
 
 export const router = createRouter({
