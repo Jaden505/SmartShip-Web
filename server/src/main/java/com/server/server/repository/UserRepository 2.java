79351b2d8384baf70a842835c5237ee8f98bfd28
Jaden505 <jadenxxxl@gmail.com>
2022-10-26 01:47:30 +0200

    Removed duplicate files

--- server/src/main/java/com/server/server/repository/UserRepository 2.java
+++
@@ -1,9 +0,0 @@
-package com.server.server.repository;
-
-import com.server.server.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findUserByEmail(String email);
-
-}
