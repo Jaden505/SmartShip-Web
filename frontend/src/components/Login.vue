0cf06e826297a0ab9206fa24c02ce508961c0a54
Bartek Tynior <btynior@gmail.com>
2022-11-01 00:05:15 +0100

    Dashboard styling

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -4,10 +4,10 @@
     <div class="flex flex-col lg:flex-row">
       <div class="mb-3 lg:max-w-lg lg:pr-5 lg:mb-0">
         <div class="max-w-xl mb-6">
-          <h2 class="title text-transparent text-center bg-clip-text bg-gradient-to-r from-regular-blue to-light-blue max-w-lg mb-6 text-5xl font-sans font-bold tracking-tight sm:text-4xl md:text-6xl lg:text-7xl sm:leading-none lg:text-start sm:text-start md:text-start">
+          <h2 class="title text-transparent text-center bg-clip-text bg-gradient-to-r from-blue-regular to-blue-light max-w-lg mb-6 text-5xl font-sans font-bold tracking-tight sm:text-4xl md:text-6xl lg:text-7xl sm:leading-none lg:text-start sm:text-start md:text-start">
             {{title}}
           </h2>
-          <p class="subtitle text-light-text text-center font-sans text-2xl sm:text-2xl md:text-3xl lg:text-3xl md:text-lg lg:text-start sm:text-start md:text-start">
+          <p class="subtitle text-white-text text-center font-sans text-2xl sm:text-2xl md:text-3xl lg:text-3xl md:text-lg lg:text-start sm:text-start md:text-start">
             {{subtitle}}
           </p>
         </div>
@@ -28,7 +28,7 @@
               </div>
 
               <div class="mt-4">
-                <button class="w-full font-bold px-4 py-2 tracking-wide text-dark-blue rounded-md bg-gradient-to-r from-regular-blue to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80">
+                <button class="w-full font-bold px-4 py-2 tracking-wide text-blue-dark rounded-md bg-gradient-to-r from-blue-regular to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80">
                   Sign in
                 </button>
               </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,11 +6,26 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Montserrat', 'sans-serif']
+      },
       colors: {
-        'regular-blue': '#0061FF',
-        'light-blue': "#60EFFF",
-        'light-text': "#E0E1DD",
-        'dark-blue': "#00111C"
+        blue: {
+          'regular': '#0061FF',
+          'light': "#60EFFF",
+          'dark': "#00111C",
+          'card-blue': '#002C48',
+          'light-card': '#15415E'
+        },
+        white: {
+          'text': '#E0E1DD',
+          'disabled': '#898A86'
+        },
+        red: {
+          'regular': '#FF0707',
+          'pink': '#FF00D5'
+        },
+
       }
     },
   },
