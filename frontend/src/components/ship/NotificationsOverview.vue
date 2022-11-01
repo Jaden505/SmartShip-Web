4e60a753f27adaa8837e5bc8f637b26865106630
Milos Mihajlovic <Milos.mihajlovic@hva.nl>
2022-11-01 23:59:59 +0100

    styling

--- frontend/src/components/ship/NotificationsOverview.vue
+++ frontend/src/components/ship/NotificationsOverview.vue
@@ -1,21 +1,103 @@
 <template>
-<div class = "flex-container">
-  <div class="flex-box">
-    Box 1
-  </div>
+  <div class = "flex-container">
 
-  <div class="flex-box">
-    Box 2
-  </div>
+    <div class="flex-box">
+      <div class="Parameters">Parameter:</div>
+      <div class="Parameters">Category:</div>
+      <div class="Parameters">Real time value: </div>
+      <div class="Parameters">setted up value:</div>
 
-  <div class="flex-box">
-    Box 3
-  </div>
+      <div class="flex">
+        <input type="button" class="flex-child" id="slide_start_button">
+        <input type="button" class="flex-child" id="slide_stop_button">
+      </div>
+    </div>
 
-  <div class="flex-box">
-    Box 4
+    <div class="flex-box">
+      <div class="Parameters">Parameter:</div>
+      <div class="Parameters">Category:</div>
+      <div class="Parameters">Real time value: </div>
+      <div class="Parameters">setted up value:</div>
+
+      <div class="flex">
+        <input type="button" class="flex-child" id="slide_start_button">
+        <input type="button" class="flex-child" id="slide_stop_button">
+      </div>
+    </div>
+
+    <div class="flex-box">
+      <div class="Parameters">Parameter:</div>
+      <div class="Parameters">Category:</div>
+      <div class="Parameters">Real time value: </div>
+      <div class="Parameters">setted up value:</div>
+
+      <div class="flex">
+        <input type="button" class="flex-child" id="slide_start_button">
+        <input type="button" class="flex-child" id="slide_stop_button">
+      </div>
+    </div>
+
+    <div class="flex-box">
+      <div class="Parameters">Parameter:</div>
+      <div class="Parameters">Category:</div>
+      <div class="Parameters">Real time value: </div>
+      <div class="Parameters">setted up value:</div>
+
+      <div class="flex">
+        <input type="button" class="flex-child" id="slide_start_button">
+        <input type="button" class="flex-child" id="slide_stop_button">
+      </div>
+    </div>
+
+    <div class="flex-box">
+      <div class="Parameters">Parameter:</div>
+      <div class="Parameters">Category:</div>
+      <div class="Parameters">Real time value: </div>
+      <div class="Parameters">setted up value:</div>
+
+      <div class="flex">
+        <input type="button" class="flex-child" id="slide_start_button">
+        <input type="button" class="flex-child" id="slide_stop_button">
+      </div>
+    </div>
+
+    <div class="flex-box">
+      <div class="Parameters">Parameter:</div>
+      <div class="Parameters">Category:</div>
+      <div class="Parameters">Real time value: </div>
+      <div class="Parameters">setted up value:</div>
+
+      <div class="flex">
+        <input type="button" class="flex-child" id="slide_start_button">
+        <input type="button" class="flex-child" id="slide_stop_button">
+      </div>
+    </div>
+
+    <div class="flex-box">
+      <div class="Parameters">Parameter:</div>
+      <div class="Parameters">Category:</div>
+      <div class="Parameters">Real time value: </div>
+      <div class="Parameters">setted up value:</div>
+
+      <div class="flex">
+        <input type="button" class="flex-child" id="slide_start_button">
+        <input type="button" class="flex-child" id="slide_stop_button">
+      </div>
+    </div>
+
+    <div class="flex-box">
+      <div class="Parameters">Parameter:</div>
+      <div class="Parameters">Category:</div>
+      <div class="Parameters">Real time value: </div>
+      <div class="Parameters">setted up value:</div>
+
+      <div class="flex">
+        <input type="button" class="flex-child" id="slide_start_button">
+        <input type="button" class="flex-child" id="slide_stop_button">
+      </div>
+    </div>
+
   </div>
-</div>
 </template>
 
 <script>
@@ -40,15 +122,52 @@ export default {
   justify-content: center;
 }
 .flex-box{
-  width: 200px;
-  height: 200px;
+  width: 320px;
+  height: 170px;
   background-color: #002C60;
   color: #0061FF;
-  font-size: 25px;
-  text-align: center;
-  line-height: 200px;
   border-radius: 20px;
   margin: 20px;
+  box-shadow: 0 3px 10px rgb(0 0 0 / 2);
+}
+
+.Parameters:first-child {
+  padding-top: 10px;
+}
+
+.Parameters{
+  margin-left: 15px;
+  font-weight: 900;
+}
+
+
+
+/*new*/
+
+.flex {
+  display: -webkit-box;
+  display: -moz-box;
+  display: -ms-flexbox;
+  display: -webkit-flex;
+  display: flex;
+}
+
+.flex-child:first-child {
+  background-color: #1CD0FF;
+  margin-left: 48%;
+}
+
+.flex-child{
+  border: none;
+  padding: 4px;
+  text-decoration: none;
+  display: inline-block;
+  margin-top: 20px;
+  float: left;
+  background-color: deeppink;
+  width: 22%;
+  border-radius: 8px;
+  margin-left: 10px;
 }
 
 </style>
