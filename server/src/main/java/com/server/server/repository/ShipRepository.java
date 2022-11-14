0bc6c4bd367238040e366d433e8bec075d7599b2
Jaden505 <jadenxxxl@gmail.com>
2022-11-15 00:01:19 +0100

    Updated database dump. Fixed bugs in Ship endpoint.

--- server/src/main/java/com/server/server/repository/ShipRepository.java
+++ server/src/main/java/com/server/server/repository/ShipRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ShipRepository extends JpaRepository<Ship, Long> {
+public interface ShipRepository extends JpaRepository<Ship, Integer> {
 
 }
