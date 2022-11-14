b1595b677de209ede2daa4fb57ba66e28ef213a1
raihanJa <raihanja@gmail.com>
2022-11-14 20:23:24 +0100

    get users endpoint

--- server/src/main/java/com/server/server/model/User.java
+++ server/src/main/java/com/server/server/model/User.java
@@ -6,13 +6,31 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-@Entity @Data @NoArgsConstructor @AllArgsConstructor
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "user")
 public class User {
-   @Id @GeneratedValue(strategy = GenerationType.AUTO)
+   @Id
+   @GeneratedValue(strategy = GenerationType.AUTO)
    private Long id;
+
+   @Column(name = "name")
    private String name;
+
+   @Column(name = "email")
    private String email;
+
+   @Column(name = "password")
    private String password;
-   @ManyToOne
-   private Role role = new Role();
+
+   @Column(name = "username")
+   private String username;
+
+   @Column(name = "role_id")
+   private int role_id;
+
+//   @ManyToOne
+//   private Role role = new Role();
 }
