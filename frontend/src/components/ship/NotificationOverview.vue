d9f4eab3215df4ff8b2e5943031e39eef80f0d7e
Milos Mihajlovic <Milos.mihajlovic@hva.nl>
2022-11-16 12:58:19 +0100

    NotificationOverview

---
+++ frontend/src/components/ship/NotificationOverview.vue
@@ -0,0 +1,15 @@
+<template>
+<h1>hello</h1>
+</template>
+
+<script>
+export default {
+  name: "NotificationOverview"
+}
+</script>
+
+<style scoped>
+body{
+  background-color: white;
+}
+</style>
