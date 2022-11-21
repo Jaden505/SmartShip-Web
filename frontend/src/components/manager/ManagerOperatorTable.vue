372fe384df5ca97b4ffd2700f51a36964a90540e
raihanJa <raihanja@gmail.com>
2022-11-21 21:35:54 +0100

    popup form

--- frontend/src/components/manager/ManagerOperatorTable.vue
+++ frontend/src/components/manager/ManagerOperatorTable.vue
@@ -1,95 +1,127 @@
 <template>
-<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
   <table>
     <thead>
-      <tr>
-        <th>Operator Id</th>
-        <th>Operator Name</th>
-        <th>Operator Email</th>
-        <th>Assigned Ship Id</th>
-        <th></th>
-      </tr>
+    <tr>
+      <th>Operator Id</th>
+      <th>Operator Name</th>
+      <th>Operator Email</th>
+      <th>Assigned Ship Id</th>
+      <th></th>
+    </tr>
     </thead>
     <tbody>
-      <tr v-for="(operator, index) in users" :key="index">
-            <td>{{ operator.id }}</td>
-            <td>{{ operator.username }}</td>
-            <td>{{ operator.email }}</td>
-            <td>{{ operator.shipID }}</td>
-            <td class="material-icons">edit</td>
-            <td class="material-icons">delete</td>
-        </tr>
+    <tr v-for="(operator, index) in users" :key="index">
+      <td>{{ operator.id }}</td>
+      <td>{{ operator.username }}</td>
+      <td>{{ operator.email }}</td>
+      <td>{{ operator.shipID }}</td>
+      <td class="material-icons" @click="TogglePopup('buttonTrigger'); this.operator = operator">edit</td>
+      <td class="material-icons">delete</td>
+    </tr>
     </tbody>
   </table>
+
+
+  <editForm
+    v-if="popupTrigger.buttonTrigger"
+    :TogglePopup="() => TogglePopup('buttonTrigger')"
+    :operator = this.operator
+  />
 
 </template>
 
 <script>
 import UserService from "../../services/user.service";
+import editForm from "@/components/manager/editForm";
+import { ref } from 'vue';
 
 export default {
   name: "ManagerTable",
+  components: {
+    editForm
+  },
 
   mounted() {
-        this.getUsers();
-    },
+    this.getUsers();
+  },
 
-    data() {
-        return {
-            users: [],
-        }
-    },
-
-    methods: {
-        getUsers() {
-          UserService.getUsersByRole(1) // Role operator
-            .then(response => {
-                this.users = response.data;
-                console.log(response.data);
-            })
-            .catch(e => {
-                console.log(e);
-        });
-        }
+  data() {
+    return {
+      users: [],
+      operator: null
     }
+  },
+
+  setup(){
+   const popupTrigger = ref({
+      buttonTrigger: false
+    });
+
+   const TogglePopup = (trigger) => {
+     popupTrigger.value[trigger] = !popupTrigger.value[trigger]
+   };
+
+   return {
+     popupTrigger,
+     TogglePopup
+   }
+  },
+
+  methods: {
+    getUsers() {
+      UserService.getUsersByRole(1) // Role operator
+          .then(response => {
+            this.users = response.data;
+            console.log(response.data);
+          })
+          .catch(e => {
+            console.log(e);
+          });
+    },
+
+    toggle(operator){
+      this.operator = operator
+    }
+  }
 }
 </script>
 
 <style scoped>
 
-table{
+table {
   width: 90%;
   border: solid black 2px;
   margin: 1% auto 0 auto;
 }
 
-thead{
+thead {
   background-color: #163b7a;
   border-bottom: 2px solid black;
 }
 
-thead th{
+thead th {
   padding: 1%;
   font-size: 22px;
   color: lightgrey;
 }
 
-tbody td{
+tbody td {
   padding: 1%;
   font-size: 20px;
 }
 
-tbody tr{
+tbody tr {
   background-color: white;
 }
 
-tbody tr:hover{
+tbody tr:hover {
   background-color: white;
   opacity: 0.8;
 }
 
-tbody tr:nth-child(even){
+tbody tr:nth-child(even) {
   background-color: deepskyblue;
 }
 
---
+++ frontend/src/components/manager/editForm.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="popup" >
+    <div class="popup-inner">
+      <h1>Edit Operator</h1>
+      <form>
+        <label>Operator Name</label><br>
+        <input type="text" v-model="current.username"><br>
+        <label>Email</label><br>
+        <input type="text" v-model="current.email"><br/>
+      </form>
+      <button class="popup-close" @click="TogglePopup()">
+        Update
+      </button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "editForm",
+  props: ['TogglePopup', 'operator'],
+
+  data(){
+    return {
+      current: this.operator
+    }
+  }
+}
+</script>
+
+<style scoped>
+.popup {
+  position: fixed;
+  top: -20%;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 99;
+  background-color: rgba(0, 0, 0, 0.6);
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.popup-inner {
+  background: #163b7a;
+  padding: 3%;
+}
+
+h1{
+  color: white;
+}
+
+label{
+  width: 100%;
+  text-align: center;
+  color: white;
+}
+
+input{
+  width: 100%;
+  padding: 1%;
+}
+
+button{
+  margin-top: 10%;
+  width: 100%;
+}
+</style>
