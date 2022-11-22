56fb0a85215d3016d48c1e7d4cb5977bd58d2d4c
raihanJa <raihanja@gmail.com>
2022-11-22 18:13:32 +0100

    frontend

---
+++ frontend/src/components/manager/ManagerDashboard.vue
@@ -0,0 +1,46 @@
+<template>
+  <ManagerNav></ManagerNav>
+
+  <div class="welcome">
+    <h1>Welcome Manager</h1>
+  </div>
+
+  <router-view></router-view>
+</template>
+
+<script>
+import ManagerNav from "@/components/manager/ManagerNav";
+
+export default {
+  name: "ManagerDashboard",
+  components: {
+    ManagerNav,
+  },
+
+  data(){
+    return{
+      isActive: false
+    }
+  },
+
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped>
+
+.welcome{
+  margin-top: 1%;
+  margin-bottom: 1%;
+}
+
+h1{
+  color: white;
+  text-align: center;
+}
+
+
+
+</style>
