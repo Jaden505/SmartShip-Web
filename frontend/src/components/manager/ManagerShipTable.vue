d57a3c07600060b155fc2ff76d5ca98852b08ecc
Jaden505 <jadenxxxl@gmail.com>
2022-11-26 16:23:06 +0100

    Stash

--- frontend/src/components/manager/ManagerShipTable.vue
+++ frontend/src/components/manager/ManagerShipTable.vue
@@ -20,7 +20,7 @@
       <td>{{ ship.id }}</td>
       <td>{{ ship.name }}</td>
       <td>{{ getStatusNameById(ship.status) }}</td>
-      <td>{{ getUsersByShipId(ship.id) }}</td>
+      <td>{{ getUsersByShipId(ship.id, index) }}</td>
       <td class="material-icons" @click="TogglePopup('buttonTriggerEdit'); this.ship = ship">edit</td>
       <td class="material-icons" @click="deleteShip(ship.id)">delete</td>
     </tr>
@@ -142,6 +142,7 @@ export default {
       StatusService.get()
           .then(response => {
             this.statuses = response.data;
+            console.log(this.statuses)
             console.log(response.data);
           })
           .catch(e => {
@@ -149,8 +150,9 @@ export default {
           });
     },
 
-    getStatusNameById(id) {
-      let status_name = (this.statuses[id-1].status).toString();
+    getStatusNameById(id, index) {
+      console.log(this.statuses, id)
+      let status_name = (this.statuses[index].status).toString();
       status_name = status_name.charAt(0) + status_name.substring(1).toLowerCase(); // Make lowercase except for first letter
       return status_name;
     }
