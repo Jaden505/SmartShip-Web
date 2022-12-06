3e452d9019c4f4515ec4032c549748d123cf8d99
Jaden505 <jadenxxxl@gmail.com>
2022-12-06 18:30:55 +0100

    Added edit dashboard button toggling edit buttons

--- frontend/src/components/ship/PowerUsage.vue
+++ frontend/src/components/ship/PowerUsage.vue
@@ -1,11 +1,16 @@
 <template>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+
   <div v-motion
        :initial="{ opacity: 0, y: 100 }"
        :enter="{ opacity: 1, y: 0, scale: 1 }"
        :variants="{ custom: { scale: 2 } }"
        :delay="100">
+    <button @click="this.isEditing = !this.isEditing" class="edit-dashboard text-white bg-blue-light-card focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center text-white-text">Edit dashboard</button>
+
     <div class="grid grid-cols-1 p-4 space-y-8 lg:gap-8 lg:space-y-0 lg:grid-cols-4">
       <div class="col-span-2 shadow-md bg-blue-card-blue rounded-md">
+        <td class="material-icons py-4 px-6" :class="{hidden: !isEditing}">fullscreen_exit</td>
         <div class="flex items-center justify-between p-4">
           <h4 class="text-xl font-semibold text-white-text">Startboard Engine Usage</h4>
         </div>
@@ -15,6 +20,7 @@
       </div>
 
       <div class="col-span-2 bg-blue-card-blue shadow-md rounded-md">
+        <td class="material-icons py-4 px-6" :class="{hidden: !isEditing}">fullscreen_exit</td>
         <div class="flex items-center justify-between p-4">
           <h4 class="text-xl font-semibold text-white-text">Battery Pack Levels</h4>
         </div>
@@ -26,16 +32,16 @@
 
     <div class="grid grid-cols-1 p-4 space-y-8 lg:gap-8 lg:space-y-0 lg:grid-cols-2">
       <!-- CARD -->
-<!--      <div class="bg-blue-card-blue shadow-md rounded-md">-->
-<!--        &lt;!&ndash; CARD HEADER &ndash;&gt;-->
-<!--        <div class="flex items-center justify-between p-4">-->
-<!--          <h4 class="text-xl font-semibold text-white-text">Doughnut Chart</h4>-->
-<!--        </div>-->
-<!--        &lt;!&ndash; CHART &ndash;&gt;-->
-<!--        <div class="relative p-4 h-72">-->
+      <!--      <div class="bg-blue-card-blue shadow-md rounded-md">-->
+      <!--        &lt;!&ndash; CARD HEADER &ndash;&gt;-->
+      <!--        <div class="flex items-center justify-between p-4">-->
+      <!--          <h4 class="text-xl font-semibold text-white-text">Doughnut Chart</h4>-->
+      <!--        </div>-->
+      <!--        &lt;!&ndash; CHART &ndash;&gt;-->
+      <!--        <div class="relative p-4 h-72">-->
 
-<!--        </div>-->
-<!--      </div>-->
+      <!--        </div>-->
+      <!--      </div>-->
       <div class="grid grid-cols-2 gap-8">
         <div class="items-center text-center p-4 shadow-md bg-white rounded-md bg-blue-card-blue">
           <div>
@@ -88,10 +94,32 @@ export default {
   components: {
     BatteryInfo,
     EngineUsage
+  },
 
-  }
+  data() {
+    return {
+      isEditing: false
+    }
+  },
 }
 </script>
 
 <style scoped>
+.edit-dashboard {
+  margin-left: 18px;
+}
+
+.material-icons {
+  float: right;
+  background-color: #0d6efd;
+  background-image:
+      linear-gradient(
+          to right,
+          #0d6efd, deepskyblue
+      );
+}
+
+.hidden {
+  visibility: hidden;
+}
 </style>
