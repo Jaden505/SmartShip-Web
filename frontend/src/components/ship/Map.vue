67191c0c67b0bd6217acf6f9f2da5f6957fcdd61
Milos Mihajlovic <Milos.mihajlovic@hva.nl>
2022-12-08 00:13:13 +0100

    heb mn best gedaan, er is progress maar die map mist nog steeds een import apperently

--- frontend/src/components/ship/Map.vue
+++ frontend/src/components/ship/Map.vue
@@ -1,36 +1,95 @@
 <template>
-  <head>
-    <meta charset='utf-8' />
-    <title>PTV Vector Map Restrictions Tutorial</title>
-    <meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no' />
-    <link rel="stylesheet"  type="text/css" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
-    <link rel="stylesheet" type="text/css" href="https://unpkg.com/maplibre-gl@2.1.9/dist/maplibre-gl.css" />
-
-    <style>
-      body { margin:0; padding:0; }
-      #map { position:absolute; top:0; bottom:0; width:100%; }
-    </style>
-  </head>
+<!--  <head>-->
+<!--    <meta charset='utf-8' />-->
+<!--    <title>PTV Vector Map Restrictions Tutorial</title>-->
+<!--    <meta name='viewport' content='initial-scale=1,maximum-scale=1,user-scalable=no' />-->
+<!--    <link rel="stylesheet"  type="text/css" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />-->
+<!--    <link rel="stylesheet" type="text/css" href="https://unpkg.com/maplibre-gl@2.1.9/dist/maplibre-gl.css" />-->
+<!--  </head>-->
   <body>
 
-  <div id="map"></div>
-
-  <script type="text/javascript" src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js" ></script>
-  <script type="text/javascript" src="https://unpkg.com/maplibre-gl@2.1.7/dist/maplibre-gl.js" ></script>
-  <script type="text/javascript" src="https://unpkg.com/@maplibre/maplibre-gl-leaflet@0.0.15/leaflet-maplibre-gl.js" ></script>
-  <script type="text/javascript" src="https://unpkg.com/leaflet-ptv-developer@1.0.1/dist/leaflet-ptv-developer.js" ></script>
-  <script type="text/javascript">
-    /* The JS code below goes here */
-  </script>
+  <div class="map" id="map"></div>
   </body>
 </template>
 
 <script>
-import AlarmService from "@/services/AlarmService";
+
+import leaflet from "leaflet";
+import maplibreGL from "maplibre-gl";
+
+
+import leafletDev from "leaflet-ptv-developer";
+
+// var scripts = [
+//   "https://unpkg.com/leaflet@1.7.1/dist/leaflet.js", "https://unpkg.com/maplibre-gl@2.1.7/dist/maplibre-gl.js", "https://unpkg.com/@maplibre/maplibre-gl-leaflet@0.0.15/leaflet-maplibre-gl.js", "https://unpkg.com/leaflet-ptv-developer@1.0.1/dist/leaflet-ptv-developer.js"];
+// scripts.forEach(script => {
+//   let tag = document.createElement('script');
+//   tag.setAttribute("scr", script);
+//   document.head.appendChild(tag);
+// });
+//
+// console.log(scripts[0])
 
 export default {
-  name: "Map"
-},
+  name: "Map",
+
+  mounted(){
+    const API_KEY = 'RVVfOGFhMDk3MTFkNDM4NDkxOGExZTQzNWRmZmMwNTI5MzI6ZTBlMTZjYzItMWQ3Yi00YzYxLTk2NTAtYzc1Y2QyOTFiYWUz';
+
+    const coordinates = leaflet.latLng(49.012, 8.4044); // Karlsruhe, Germany
+    const map = leaflet.map('map', {
+      fullscreenControl: true
+    }).setView(coordinates, 17);
+
+    maplibreGL.setRTLTextPlugin(
+        'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js',
+        null,
+        true
+    );
+
+    var vectorLayer = maplibreGL({
+      attribution: '&copy; ' + new Date().getFullYear() + ' PTV Group, HERE',
+      interactive:false,
+      maxZoom: 18,
+      style: 'https://vectormaps-resources.myptv.com/styles/latest/standard.json',
+      transformRequest: (url, resourceType) => {
+        if (resourceType === 'Tile' && url.startsWith('https://api.myptv.com')) {
+          return {
+            url: url + '?apiKey=' + "RVVfOGFhMDk3MTFkNDM4NDkxOGExZTQzNWRmZmMwNTI5MzI6ZTBlMTZjYzItMWQ3Yi00YzYxLTk2NTAtYzc1Y2QyOTFiYWUz"
+          }
+        }
+      }
+    }).addTo(map);
+  }
+
+    // const API_KEY = 'RVVfOGFhMDk3MTFkNDM4NDkxOGExZTQzNWRmZmMwNTI5MzI6ZTBlMTZjYzItMWQ3Yi00YzYxLTk2NTAtYzc1Y2QyOTFiYWUz';
+    //
+    // const coordinates = L.latLng(49.012, 8.4044); // Karlsruhe, Germany
+    // const map = L.map('map', {
+    //   fullscreenControl: true
+    // }).setView(coordinates, 17);
+    //
+    // maplibregl.setRTLTextPlugin(
+    //     'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js',
+    //     null,
+    //     true
+    // );
+    //
+    // var vectorLayer = L.maplibreGL({
+    //   attribution: '&copy; ' + new Date().getFullYear() + ' PTV Group, HERE',
+    //   interactive:false,
+    //   maxZoom: 18,
+    //   style: 'https://vectormaps-resources.myptv.com/styles/latest/standard.json',
+    //   transformRequest: (url, resourceType) => {
+    //     if (resourceType === 'Tile' && url.startsWith('https://api.myptv.com')) {
+    //       return {
+    //         url: url + '?apiKey=' + API_KEY
+    //       }
+    //     }
+    //   }
+    // }).addTo(map);
+    //
+}
 
 // mounted(){
 //   // let mapScript1 = document.createElement('Mapscript1')
@@ -50,46 +109,52 @@ export default {
 //   // document.head.appendChild(mapScript4)
 // },
 
-created() {
-  var scripts = [
-      "https://unpkg.com/leaflet@1.7.1/dist/leaflet.js", "https://unpkg.com/maplibre-gl@2.1.7/dist/maplibre-gl.js", "https://unpkg.com/@maplibre/maplibre-gl-leaflet@0.0.15/leaflet-maplibre-gl.js", "https://unpkg.com/leaflet-ptv-developer@1.0.1/dist/leaflet-ptv-developer.js"];
-  scripts.forEach(script => {
-    let tag = document.createElement('script');
-    tag.setAttribute("scr", script);
-    document.head.appendChild(tag);
-  })
-}
-
-methods: {
-  const API_KEY = 'YOUR_API_KEY';
-
-  const coordinates = L.latLng(49.012, 8.4044); // Karlsruhe, Germany
-  const map = L.map('map', {
-    fullscreenControl: true
-  }).setView(coordinates, 17);
-
-  maplibregl.setRTLTextPlugin(
-      'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js',
-      null,
-      true
-  );
-
-  var vectorLayer = L.maplibreGL({
-    attribution: '&copy; ' + new Date().getFullYear() + ' PTV Group, HERE',
-    interactive:false,
-    maxZoom: 18,
-    style: 'https://vectormaps-resources.myptv.com/styles/latest/standard.json',
-    transformRequest: (url, resourceType) => {
-      if (resourceType === 'Tile' && url.startsWith('https://api.myptv.com')) {
-        return {
-          url: url + '?apiKey=' + API_KEY
-        }
-      }
-    }
-  }).addTo(map);
-}
+// created() {
+//   var scripts = [
+//       "https://unpkg.com/leaflet@1.7.1/dist/leaflet.js", "https://unpkg.com/maplibre-gl@2.1.7/dist/maplibre-gl.js", "https://unpkg.com/@maplibre/maplibre-gl-leaflet@0.0.15/leaflet-maplibre-gl.js", "https://unpkg.com/leaflet-ptv-developer@1.0.1/dist/leaflet-ptv-developer.js"];
+//   scripts.forEach(script => {
+//     let tag = document.createElement('script');
+//     tag.setAttribute("scr", script);
+//     document.head.appendChild(tag);
+//   })
+// }
+// methods: {
+//   const API_KEY = 'YOUR_API_KEY';
+//
+//   const coordinates = L.latLng(49.012, 8.4044); // Karlsruhe, Germany
+//   const map = L.map('map', {
+//     fullscreenControl: true
+//   }).setView(coordinates, 17);
+//
+//   maplibregl.setRTLTextPlugin(
+//       'https://api.mapbox.com/mapbox-gl-js/plugins/mapbox-gl-rtl-text/v0.2.3/mapbox-gl-rtl-text.js',
+//       null,
+//       true
+//   );
+//
+//   var vectorLayer = L.maplibreGL({
+//     attribution: '&copy; ' + new Date().getFullYear() + ' PTV Group, HERE',
+//     interactive:false,
+//     maxZoom: 18,
+//     style: 'https://vectormaps-resources.myptv.com/styles/latest/standard.json',
+//     transformRequest: (url, resourceType) => {
+//       if (resourceType === 'Tile' && url.startsWith('https://api.myptv.com')) {
+//         return {
+//           url: url + '?apiKey=' + API_KEY
+//         }
+//       }
+//     }
+//   }).addTo(map);
+// }
 </script>
 
 <style scoped>
+
+.map{
+  position:absolute;
+  bottom:0;
+  width: 82%;
+  height: 90%;
+}
 
 </style>
