33f1baaf29b39f7e5510738f2f1408065d033337
Vincent <vincent.ohr@hva.nl>
2022-12-13 10:57:45 +0100

    add getmapping

--- server/src/main/java/com/server/server/controllers/SensorDataController.java
+++ server/src/main/java/com/server/server/controllers/SensorDataController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.Optional;
 
 
@@ -20,6 +21,11 @@ public class SensorDataController {
 
     @Autowired
     SensorDataRepository repo;
+
+    @GetMapping("")
+    public ResponseEntity<List<SensorData>> getAll(){
+        return ResponseEntity.ok(repo.findAll());
+    }
 
     @PostMapping("/add")
     public ResponseEntity<SensorData> addSensorData(@RequestBody SensorData body) {
