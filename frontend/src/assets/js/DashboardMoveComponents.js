4b8b76a606bb7777fbd1465724292607912eb3e0
Jaden505 <jadenxxxl@gmail.com>
2022-12-17 11:47:13 +0100

    Fixed bug no components display

--- frontend/src/assets/js/DashboardMoveComponents.js
+++ frontend/src/assets/js/DashboardMoveComponents.js
@@ -12,7 +12,8 @@ export class DashboardMoveComponents {
     dropHandler(ev, swap_component, components) {
         // Cant drop on itself
         const dropped_container = ev.target.parentNode.parentNode;
-        if (!dropped_container.classList.contains('droppable') || !this.moving_element.classList.contains('droppable') || dropped_container === this.moving_element) return;
+        if (!dropped_container.classList.contains('droppable') || !this.moving_element.classList.contains('droppable') ||
+            dropped_container === this.moving_element) return components;
 
         let component_index = components.indexOf(this.component);
         let swap_component_index = components.indexOf(swap_component);
