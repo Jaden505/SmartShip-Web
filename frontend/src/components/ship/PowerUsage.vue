8b839613d9c4bf0197b5dfb8f044176f00ec1b26
Jaden505 <jadenxxxl@gmail.com>
2022-12-17 11:51:01 +0100

    Reload page after save

--- frontend/src/components/ship/PowerUsage.vue
+++ frontend/src/components/ship/PowerUsage.vue
@@ -16,7 +16,7 @@
         <a v-for="(component, index) in addableComponents" :key="index" @click="switchDisplayComponent(component)">{{component.name}}</a>
       </div>
     </div>
-    <button :class="{hidden: !isEditing}" @click="setComponents()"
+    <button :class="{hidden: !isEditing}" @click="setComponents(); this.$router.go()"
             class="edit-dashboard text-white bg-blue-light-card focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center text-white-text drop-btn">Save</button>
 
     <div class="grid grid-cols-1 p-4 space-y-8 lg:gap-8 lg:space-y-0 lg:grid-cols-4 comp-wrapper">
