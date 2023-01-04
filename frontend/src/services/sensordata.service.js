1da5e5f77faf6eca380288c43be4fc384df911a4
Jaden505 <jadenxxxl@gmail.com>
2023-01-04 23:42:41 +0100

    Added backend endpoint for sensordata. Already tested and working

--- frontend/src/services/sensordata.service.js
+++ frontend/src/services/sensordata.service.js
@@ -1,17 +1,17 @@
 import axios from "axios";
 import authHeader from "@/services/auth-header";
 
-const API_URL = process.env.VUE_APP_API_URL + '/api/test/sensordata';
+const API_URL = process.env.VUE_APP_API_URL + '/api/test/sensorData';
 
 class SensorDataService {
     getAll() {
-        return axios.get(API_URL + "/all", {
+        return axios.get(API_URL + "/", {
             headers: authHeader()
         });
     }
 
-    get(id){
-        return axios.get(API_URL + '/' + id, {
+    getByShipId(ship_id){
+        return axios.get(API_URL + '/' + ship_id, {
             headers: authHeader()
         })
     }
--- server/src/main/java/com/server/server/repository/SensorDataRepository.java
+++ server/src/main/java/com/server/server/repository/SensorDataRepository.java
@@ -3,5 +3,8 @@ package com.server.server.repository;
 import com.server.server.model.SensorData;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface SensorDataRepository extends JpaRepository<SensorData, Integer> {
+    List<SensorData> findByShipId(String shipId);
 }
