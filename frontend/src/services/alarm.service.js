4d84430e0b0ebd4b0d7ab3fc5aac965b6f9ea9a7
joliw <joliweibolt@gmail.com>
2023-01-18 21:56:20 +0100

    changed alarm service

--- frontend/src/services/alarm.service.js
+++ frontend/src/services/alarm.service.js
@@ -1,22 +1,22 @@
-const axios = require('axios');
+import axios from "axios";
 import authHeader from "@/services/auth-header";
 const API_URL =  process.env.VUE_APP_API_URL +"/api/test/Alarms"
-    let user = JSON.parse(localStorage.getItem('user'));
+let user = JSON.parse(localStorage.getItem('user'));
 
-export class AlarmService {
-    static getAll() {
+class AlarmService {
+    getAll() {
         return axios.get(API_URL, {
             headers: authHeader()
         });
     }
 
-     static delete(index) {
+     delete(index) {
         return axios.delete(API_URL + "/" + index, {
             headers: authHeader()
         });
     }
 
-    static postNotification(alarm) {
+    postNotification(alarm) {
         let notification = JSON.stringify(alarm)
         return axios.post(API_URL + "/send", notification, {
             headers: {
@@ -26,7 +26,7 @@ export class AlarmService {
         });
     }
 
-    static addAlarm(alarm) {
+    addAlarm(alarm) {
         let newAlarm = JSON.stringify(alarm)
         return axios.post(API_URL,
             newAlarm, {
@@ -37,47 +37,4 @@ export class AlarmService {
             });
     }
 }
-
-// const axios = require('axios');
-// import authHeader from "@/services/auth-header";
-//
-// const API_URL_ALARMS = "http://localhost:8085/api/test/Alarms";
-// let user = JSON.parse(localStorage.getItem('user'));
-//
-// class AlarmService {
-//     async getAll() {
-//         return await axios.get(API_URL_ALARMS, {
-//             headers: authHeader()
-//         });
-//     }
-//
-//     async delete(index) {
-//         return await axios.delete(API_URL_ALARMS + "/" + index, {
-//             headers: authHeader()
-//         });
-//     }
-//
-//     async postNotification(alarm) {
-//         let notification = JSON.stringify(alarm)
-//         return await axios.post(API_URL_ALARMS + "/send", notification, {
-//             headers: {
-//                 "Authorization": 'Bearer ' + user.accessToken,
-//                 "Content-Type": "application/json"
-//             }
-//         });
-//     }
-//
-//     async addAlarm(alarm) {
-//         let newAlarm = JSON.stringify(alarm)
-//         return await axios.post(API_URL_ALARMS,
-//             newAlarm, {
-//                 headers: {
-//                     "Authorization": 'Bearer ' + user.accessToken,
-//                     "Content-Type": "application/json"
-//                 }
-//             });
-//     }
-// }
-//
-//
-// export default new AlarmService()
+export default new AlarmService()
